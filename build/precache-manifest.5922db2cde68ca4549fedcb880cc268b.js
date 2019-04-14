a86827a5edded36fdd147d1df5f820ada73d5933
PlasmaIntec <alphaintec@gmail.com>
2019-04-14 08:55:26 -0700

    Upload build

---
+++ build/precache-manifest.5922db2cde68ca4549fedcb880cc268b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfb5d1fd67f324ca726b",
+    "url": "/static/css/main.e20d8c0b.chunk.css"
+  },
+  {
+    "revision": "cfb5d1fd67f324ca726b",
+    "url": "/static/js/main.b3ee2ab4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a043d3421f89dcc2d163",
+    "url": "/static/js/2.06593c0e.chunk.js"
+  },
+  {
+    "revision": "6b0508574d0962dae9b83caf143c8402",
+    "url": "/index.html"
+  }
+];
